38c6c38e6cd6b96472640671bbb7f5f0da7d8950
Johannes Schneider <johannesschneider@outlook.com>
2021-05-24 17:31:03 +0200

    [tests] Add simple test for pool allocator

--- tests/pool_allocator/pool_allocator.cpp
+++ tests/pool_allocator/pool_allocator.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 
-namespace memory_allocators {
+#include "allocators/pool_allocator/pool_allocator.hpp"
 
-// TODO: Add tests
+namespace memory_allocators::pool_allocator {
 
-} // memory_allocators
+TEST(PoolAllocator, Example1) {
+    PoolAllocator<int> MyAllocator;
+
+    int *number1 = MyAllocator.allocate(1337);
+    int *number2 = MyAllocator.allocate(42);
+
+    MyAllocator.destroy(number1);
+    MyAllocator.destroy(number2);
+}
+
+} // namespace memory_allocators::pool_allocator
